57e32b86da3d2f4172d0181bc7c4921242d0331f
Iain Forbes <i.forbes77@gmail.com>
2020-11-23 07:00:08 +0000

    Removed extra code

--- app/tests/game_test.py
+++ app/tests/game_test.py
@@ -9,8 +9,20 @@ class GameTest(unittest.TestCase):
         self.player2 = Player("Player 2", "paper")
         self.player3 = Player("Player 3", "scissors")
         self.game = Game(self.player1, self.player2)
+        self.game2 = Game(self.player1, self.player3)
+        self.game3 = Game(self.player2, self.player3)
 
-    # def test_player_1_win(self):
-    #     self.game.pvp_result(self.player1.move, self.player2.move)
-    #     self.assertEqual(self.player1.name, self.player1.name)
+    def test_game_winner_paper_win(self):
+        self.game.winner(self.player1, self.player2)
+        self.assertEqual('Player 2', self.player2.name)
+    
+
+    def test_game2_winner_rock_win(self):
+        self.game2.winner(self.player1, self.player3)
+        self.assertEqual('Player 1', self.player1.name)
+
+    def test_game2_winner_scissors_win(self):
+        self.game2.winner(self.player2, self.player3)
+        self.assertEqual('Player 3', self.player3.name)
+        
         
